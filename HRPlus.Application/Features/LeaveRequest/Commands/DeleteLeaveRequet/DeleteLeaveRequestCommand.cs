144bf2cde18d21c814f8557a924a8427e5574118
Ray Brown-Amory <r.brown-amory@sky.com>
2023-09-03 09:21:44 +0100

    creating the DeleteCommand for the Leave Request

---
+++ HRPlus.Application/Features/LeaveRequest/Commands/DeleteLeaveRequet/DeleteLeaveRequestCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace HRPlus.Application.Features.LeaveRequest.Commands.DeleteLeaveRequet
+{
+    public class DeleteLeaveRequestCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
